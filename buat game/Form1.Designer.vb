7ab3388b6188c8cbaf8fc44da3ecb25ead9a214b
kurna <kurna@DESKTOP-NE7VCVP>
2024-07-04 20:50:38 +0700

    Add project files.

---
+++ buat game/Form1.Designer.vb
@@ -0,0 +1,394 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        PictureBox1 = New PictureBox()
+        PictureBox2 = New PictureBox()
+        PictureBox3 = New PictureBox()
+        PictureBox4 = New PictureBox()
+        PictureBox5 = New PictureBox()
+        PictureBox6 = New PictureBox()
+        car = New PictureBox()
+        PictureBox7 = New PictureBox()
+        PictureBox8 = New PictureBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        RoadMover = New Timer(components)
+        RightSide = New Timer(components)
+        LeftSide = New Timer(components)
+        RacerMover1 = New Timer(components)
+        RaceMover2 = New Timer(components)
+        RaceMover3 = New Timer(components)
+        Label3 = New Label()
+        Button1 = New Button()
+        race2 = New PictureBox()
+        race1 = New PictureBox()
+        race3 = New PictureBox()
+        RaceMover4 = New Timer(components)
+        Racemover5 = New Timer(components)
+        MenuStrip1 = New MenuStrip()
+        EXITToolStripMenuItem = New ToolStripMenuItem()
+        SCORBOARDToolStripMenuItem = New ToolStripMenuItem()
+        SCOREBOARDToolStripMenuItem = New ToolStripMenuItem()
+        CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).BeginInit()
+        CType(car, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).BeginInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).BeginInit()
+        CType(race2, ComponentModel.ISupportInitialize).BeginInit()
+        CType(race1, ComponentModel.ISupportInitialize).BeginInit()
+        CType(race3, ComponentModel.ISupportInitialize).BeginInit()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' PictureBox1
+        ' 
+        PictureBox1.Image = My.Resources.Resources.awan
+        PictureBox1.Location = New Point(2, 19)
+        PictureBox1.Margin = New Padding(5)
+        PictureBox1.Name = "PictureBox1"
+        PictureBox1.Size = New Size(232, 202)
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox1.TabIndex = 0
+        PictureBox1.TabStop = False
+        ' 
+        ' PictureBox2
+        ' 
+        PictureBox2.Image = My.Resources.Resources.awan
+        PictureBox2.Location = New Point(266, 312)
+        PictureBox2.Margin = New Padding(5)
+        PictureBox2.Name = "PictureBox2"
+        PictureBox2.Size = New Size(232, 202)
+        PictureBox2.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox2.TabIndex = 1
+        PictureBox2.TabStop = False
+        ' 
+        ' PictureBox3
+        ' 
+        PictureBox3.Image = My.Resources.Resources.awan
+        PictureBox3.Location = New Point(1531, 74)
+        PictureBox3.Margin = New Padding(5)
+        PictureBox3.Name = "PictureBox3"
+        PictureBox3.Size = New Size(128, 118)
+        PictureBox3.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox3.TabIndex = 2
+        PictureBox3.TabStop = False
+        ' 
+        ' PictureBox4
+        ' 
+        PictureBox4.Image = My.Resources.Resources.awan
+        PictureBox4.Location = New Point(681, 240)
+        PictureBox4.Margin = New Padding(5)
+        PictureBox4.Name = "PictureBox4"
+        PictureBox4.Size = New Size(434, 379)
+        PictureBox4.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox4.TabIndex = 3
+        PictureBox4.TabStop = False
+        ' 
+        ' PictureBox5
+        ' 
+        PictureBox5.Image = My.Resources.Resources.awan
+        PictureBox5.Location = New Point(1492, 835)
+        PictureBox5.Margin = New Padding(5)
+        PictureBox5.Name = "PictureBox5"
+        PictureBox5.Size = New Size(301, 256)
+        PictureBox5.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox5.TabIndex = 4
+        PictureBox5.TabStop = False
+        ' 
+        ' PictureBox6
+        ' 
+        PictureBox6.Image = My.Resources.Resources.awan
+        PictureBox6.Location = New Point(-89, 835)
+        PictureBox6.Margin = New Padding(5)
+        PictureBox6.Name = "PictureBox6"
+        PictureBox6.Size = New Size(270, 235)
+        PictureBox6.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox6.TabIndex = 5
+        PictureBox6.TabStop = False
+        ' 
+        ' car
+        ' 
+        car.Image = My.Resources.Resources.pesawat_kita
+        car.Location = New Point(332, 782)
+        car.Margin = New Padding(5)
+        car.Name = "car"
+        car.Size = New Size(101, 106)
+        car.SizeMode = PictureBoxSizeMode.Zoom
+        car.TabIndex = 6
+        car.TabStop = False
+        ' 
+        ' PictureBox7
+        ' 
+        PictureBox7.Image = My.Resources.Resources.awan
+        PictureBox7.Location = New Point(514, 651)
+        PictureBox7.Margin = New Padding(5)
+        PictureBox7.Name = "PictureBox7"
+        PictureBox7.Size = New Size(307, 253)
+        PictureBox7.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox7.TabIndex = 10
+        PictureBox7.TabStop = False
+        ' 
+        ' PictureBox8
+        ' 
+        PictureBox8.Image = My.Resources.Resources.awan
+        PictureBox8.Location = New Point(996, -62)
+        PictureBox8.Margin = New Padding(5)
+        PictureBox8.Name = "PictureBox8"
+        PictureBox8.Size = New Size(254, 202)
+        PictureBox8.SizeMode = PictureBoxSizeMode.Zoom
+        PictureBox8.TabIndex = 11
+        PictureBox8.TabStop = False
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Gill Sans Ultra Bold Condensed", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(0, 62)
+        Label1.Margin = New Padding(5, 0, 5, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(257, 73)
+        Label1.TabIndex = 12
+        Label1.Text = "SCORE : 0"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Gill Sans Ultra Bold Condensed", 19.8000011F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(438, 62)
+        Label2.Margin = New Padding(5, 0, 5, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(221, 73)
+        Label2.TabIndex = 13
+        Label2.Text = "SPEED 0"
+        ' 
+        ' RoadMover
+        ' 
+        RoadMover.Enabled = True
+        RoadMover.Interval = 2
+        ' 
+        ' RightSide
+        ' 
+        RightSide.Enabled = True
+        RightSide.Interval = 2
+        ' 
+        ' LeftSide
+        ' 
+        LeftSide.Enabled = True
+        LeftSide.Interval = 2
+        ' 
+        ' RacerMover1
+        ' 
+        RacerMover1.Enabled = True
+        RacerMover1.Interval = 2
+        ' 
+        ' RaceMover2
+        ' 
+        RaceMover2.Enabled = True
+        RaceMover2.Interval = 2
+        ' 
+        ' RaceMover3
+        ' 
+        RaceMover3.Enabled = True
+        RaceMover3.Interval = 2
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.BackColor = Color.Black
+        Label3.Font = New Font("Showcard Gothic", 34.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.ForeColor = Color.Red
+        Label3.Location = New Point(94, 400)
+        Label3.Margin = New Padding(5, 0, 5, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(565, 114)
+        Label3.TabIndex = 14
+        Label3.Text = "GAME OVER"
+        Label3.Visible = False
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.LightGray
+        Button1.Font = New Font("Stencil", 16.2F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = SystemColors.ActiveCaptionText
+        Button1.Location = New Point(250, 530)
+        Button1.Margin = New Padding(5)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(266, 77)
+        Button1.TabIndex = 15
+        Button1.Text = "RESTART"
+        Button1.UseVisualStyleBackColor = False
+        Button1.Visible = False
+        ' 
+        ' race2
+        ' 
+        race2.BackColor = Color.Transparent
+        race2.Image = CType(resources.GetObject("race2.Image"), Image)
+        race2.Location = New Point(276, 67)
+        race2.Margin = New Padding(5)
+        race2.Name = "race2"
+        race2.Size = New Size(128, 298)
+        race2.SizeMode = PictureBoxSizeMode.CenterImage
+        race2.TabIndex = 16
+        race2.TabStop = False
+        ' 
+        ' race1
+        ' 
+        race1.BackColor = Color.Transparent
+        race1.Image = CType(resources.GetObject("race1.Image"), Image)
+        race1.Location = New Point(551, 370)
+        race1.Margin = New Padding(5)
+        race1.Name = "race1"
+        race1.Size = New Size(141, 384)
+        race1.SizeMode = PictureBoxSizeMode.CenterImage
+        race1.TabIndex = 17
+        race1.TabStop = False
+        ' 
+        ' race3
+        ' 
+        race3.BackColor = Color.Transparent
+        race3.Image = CType(resources.GetObject("race3.Image"), Image)
+        race3.Location = New Point(41, 682)
+        race3.Margin = New Padding(5)
+        race3.Name = "race3"
+        race3.Size = New Size(140, 363)
+        race3.SizeMode = PictureBoxSizeMode.CenterImage
+        race3.TabIndex = 18
+        race3.TabStop = False
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.BackColor = Color.Black
+        MenuStrip1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        MenuStrip1.ImageScalingSize = New Size(20, 20)
+        MenuStrip1.Items.AddRange(New ToolStripItem() {EXITToolStripMenuItem, SCORBOARDToolStripMenuItem, SCOREBOARDToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Padding = New Padding(10, 3, 0, 3)
+        MenuStrip1.Size = New Size(686, 50)
+        MenuStrip1.TabIndex = 21
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' EXITToolStripMenuItem
+        ' 
+        EXITToolStripMenuItem.BackColor = Color.Black
+        EXITToolStripMenuItem.ForeColor = Color.White
+        EXITToolStripMenuItem.Name = "EXITToolStripMenuItem"
+        EXITToolStripMenuItem.Size = New Size(98, 44)
+        EXITToolStripMenuItem.Text = "EXIT"
+        ' 
+        ' SCORBOARDToolStripMenuItem
+        ' 
+        SCORBOARDToolStripMenuItem.ForeColor = Color.White
+        SCORBOARDToolStripMenuItem.Name = "SCORBOARDToolStripMenuItem"
+        SCORBOARDToolStripMenuItem.Size = New Size(194, 44)
+        SCORBOARDToolStripMenuItem.Text = "USERNAME"
+        ' 
+        ' SCOREBOARDToolStripMenuItem
+        ' 
+        SCOREBOARDToolStripMenuItem.ForeColor = Color.White
+        SCOREBOARDToolStripMenuItem.Name = "SCOREBOARDToolStripMenuItem"
+        SCOREBOARDToolStripMenuItem.Size = New Size(228, 44)
+        SCOREBOARDToolStripMenuItem.Text = "SCOREBOARD"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.LightSteelBlue
+        ClientSize = New Size(686, 1008)
+        Controls.Add(Label1)
+        Controls.Add(MenuStrip1)
+        Controls.Add(Label3)
+        Controls.Add(Button1)
+        Controls.Add(race2)
+        Controls.Add(Label2)
+        Controls.Add(race3)
+        Controls.Add(race1)
+        Controls.Add(car)
+        Controls.Add(PictureBox6)
+        Controls.Add(PictureBox5)
+        Controls.Add(PictureBox4)
+        Controls.Add(PictureBox3)
+        Controls.Add(PictureBox2)
+        Controls.Add(PictureBox1)
+        Controls.Add(PictureBox7)
+        Controls.Add(PictureBox8)
+        MainMenuStrip = MenuStrip1
+        Margin = New Padding(5)
+        Name = "Form1"
+        Text = "AEROPLANE GLITCH"
+        CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox2, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox3, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox4, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox5, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox6, ComponentModel.ISupportInitialize).EndInit()
+        CType(car, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox7, ComponentModel.ISupportInitialize).EndInit()
+        CType(PictureBox8, ComponentModel.ISupportInitialize).EndInit()
+        CType(race2, ComponentModel.ISupportInitialize).EndInit()
+        CType(race1, ComponentModel.ISupportInitialize).EndInit()
+        CType(race3, ComponentModel.ISupportInitialize).EndInit()
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents car As PictureBox
+    Friend WithEvents PictureBox7 As PictureBox
+    Friend WithEvents PictureBox8 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RoadMover As Timer
+    Friend WithEvents RightSide As Timer
+    Friend WithEvents LeftSide As Timer
+    Friend WithEvents RacerMover1 As Timer
+    Friend WithEvents RaceMover2 As Timer
+    Friend WithEvents RaceMover3 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents race2 As PictureBox
+    Friend WithEvents race1 As PictureBox
+    Friend WithEvents race3 As PictureBox
+    Friend WithEvents RaceMover4 As Timer
+    Friend WithEvents Racemover5 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents EXITToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SCORBOARDToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SCOREBOARDToolStripMenuItem As ToolStripMenuItem
+
+End Class
